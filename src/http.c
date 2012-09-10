67740ddc1ee51c61286b73e197d2991e7289d475
los-t <los-t@ya.ru>
2012-09-10 04:59:24 +0400

    Header will not be save to file.

--- src/http.c
+++ src/http.c
@@ -1,8 +1,9 @@
 #define _GNU_SOURCE
-#include <stdio.h>
+#include <assert.h>
+#include <stdio.h> 
 #include <string.h>
 
-#include "fs.h"
+#include "fs.h" 
 #include "net.h"
 
 #include "http.h"
@@ -17,6 +18,8 @@ const char* HTTP_HDR_SP = ": ";
 const char* HTTP_HDR_HOST = "Host";
 const char* HTTP_HDR_AGENT = "User-agent";
 const char* HTTP_HDR_CONN = "Connection";
+
+const char* HTTP_RESPHDR_SP = "\r\n\r\n";
 
 ERROR_CODE http_build_request_line(char** dst, const char* method, const char* path) {
 	if (*dst)
@@ -35,9 +38,7 @@ ERROR_CODE http_build_request_header(char** dst, const char* hdr, const char* va
 		free(dst);
 
 	if (asprintf(dst, "%s%s%s", hdr, HTTP_HDR_SP, val) < 0) {
-		*dst = NULL;
-		return ERR_HTTP_BLDFAIL_HDR;
-	}
+		*dst = NULL; return ERR_HTTP_BLDFAIL_HDR; }
 
 	return ERR_NONE;
 }
@@ -82,10 +83,73 @@ ERROR_CODE http_build_request(char** dst, const Url* url) {
 	return ERR_NONE;
 }
 
+ERROR_CODE http_header_process(char* hdr) {
+	/* for now just print to stdout */
+	printf("Server reponse header:\n%s\n", hdr);
+	return ERR_NONE;
+}
+
+/* TODO: Review the algorithm if a more generic approach to special case is possible */
+const char* http_header_helper(const char* data, size_t* datasize) {
+	static char* hdr = NULL;
+	static size_t hdrlen = 0;
+	char* hdr_end = NULL;
+	size_t size = *datasize;
+
+	assert(data != NULL);
+
+	/* Special case when a delimiter was split in two chunks, avoiding reallocation */
+	if (hdr && 
+			hdr[hdrlen-1] == HTTP_RESPHDR_SP[0] &&
+			data[0] == HTTP_RESPHDR_SP[1]) {
+		http_header_process(hdr);
+		free(hdr);
+
+		(*datasize)--;
+		return data + 1;
+	}
+	
+	hdr_end = strstr(data, HTTP_RESPHDR_SP);
+	if(hdr_end)
+		/* Take care of a special case when header was already processed but delimiter arrived in next chunk */
+		size = (hdr_end != data) ? (hdr_end - data) : 0;
+
+	if (size) {
+		hdr = realloc(hdr, hdrlen + size);
+		assert(hdr != NULL);
+		strncat(hdr, data, size);
+		hdrlen = hdrlen + size;
+	}
+
+	if(hdr_end) {
+		http_header_process(hdr);
+		free(hdr);
+
+		*datasize = size;
+		return hdr_end + strlen(HTTP_RESPHDR_SP);
+	}
+
+	return NULL;
+}
+
 ERROR_CODE http_save_helper(NET_HANDLER_OBJ file, const char* data, const size_t size) {
 	File *strm = (File*)file;
+	static unsigned int am_expecting_hdr = 1;
+	const char* data_to_dump = data;
+	size_t tmpsize = size;
 
-	return fs_write(strm, data, size);
+	if (am_expecting_hdr) {
+		if (!(data_to_dump = http_header_helper(data, &tmpsize)))
+			return ERR_NONE;
+
+		am_expecting_hdr = 0;
+	}
+
+	if(!tmpsize)
+		/* Not writing anything - all went to header */
+		return ERR_NONE;
+
+	return fs_write(strm, data_to_dump, tmpsize);
 }
 
 ERROR_CODE http_get(const Url* url, const char* path) {
--- src/http.h
+++ src/http.h
@@ -4,21 +4,6 @@
 #include "error.h"
 #include "url.h"
 
-extern const char* HTTP_SP;
-extern const char* HTTP_VERSION;
-extern const char* HTTP_USERAGENT;
-
-extern const char* HTTP_MTD_GET;
-
-extern const char* HTTP_HDR_SP;
-extern const char* HTTP_HDR_HOST;
-extern const char* HTTP_HDR_AGENT;
-extern const char* HTTP_HDR_CONN;
-
-ERROR_CODE http_build_request_line(char** dst, const char* method, const char* path);
-ERROR_CODE http_build_request_header(char** dst, const char* hdr, const char* val);
-ERROR_CODE http_build_request(char** dst, const Url* url);
-
 ERROR_CODE http_get(const Url* /*url*/, const char* /*path*/);
 
 #endif // CLIHT_HTTP_H_
